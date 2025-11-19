ef612f720471f0f801c58a6f0f8923d974696df1
Olivia Lee <olivia.lee@collabora.com>
2025-11-18 18:20:03 -0800

    panvk: advertise VK_EXT_primitives_generated_query on v10+
    This was reverted in 4959f45e99e62619cf334b972337abc526632f8a because
    the extension has a dependency on transform feedback, but now that we
    have transform feedback we can enable it again.
    
    Signed-off-by: Olivia Lee <olivia.lee@collabora.com>

--- src/panfrost/vulkan/panvk_vX_physical_device.c
+++ src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -200,6 +200,7 @@ panvk_per_arch(get_physical_device_extensions)(
 #endif
       .EXT_private_data = true,
       .EXT_primitive_topology_list_restart = true,
+      .EXT_primitives_generated_query = PAN_ARCH >= 10,
       .EXT_provoking_vertex = true,
       .EXT_queue_family_foreign = true,
       .EXT_rasterization_order_attachment_access = PAN_ARCH >= 10,
@@ -604,6 +605,11 @@ panvk_per_arch(get_physical_device_features)(
       /* VK_EXT_primitive_topology_list_restart */
       .primitiveTopologyListRestart = true,
       .primitiveTopologyPatchListRestart = false,
+
+      /* VK_EXT_primitives_generated_query */
+      .primitivesGeneratedQuery = PAN_ARCH >= 10,
+      .primitivesGeneratedQueryWithRasterizerDiscard = PAN_ARCH >= 10,
+      .primitivesGeneratedQueryWithNonZeroStreams = true,
 
       /* VK_EXT_provoking_vertex */
       .provokingVertexLast = true,
