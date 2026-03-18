6ca174558d91e4567d40d9828b4bab64ea9b62af
Olivia Lee <olivia.lee@collabora.com>
2026-03-17 22:05:01 -0700

    panvk: duplicate VS pan_compile_inputs per-variant before modifying
    For the HW variant, we modify trust_varying_flat_highp_types and
    varying_layout. We need to copy the inputs struct before doing this,
    otherwise the change will be propagated to the SW variant. This is
    potentially a source of UB with varying_layout, which is a pointer to a
    local variable.
    
    Signed-off-by: Olivia Lee <olivia.lee@collabora.com>

--- src/panfrost/vulkan/panvk_vX_shader.c
+++ src/panfrost/vulkan/panvk_vX_shader.c
@@ -1430,6 +1430,8 @@ panvk_compile_shader(struct panvk_device *dev,
       for (enum panvk_vs_variant v = 0; v <= last_variant; v++) {
          struct panvk_shader_variant *variant = &shader->variants[v];
 
+         struct pan_compile_inputs variant_inputs = inputs;
+
          /* Each variant gets its own NIR. To save an extra clone, we use the
           * original NIR for the last stage.
           */
@@ -1442,9 +1444,9 @@ panvk_compile_shader(struct panvk_device *dev,
                          state, &shader->desc_info, false);
 
 #if PAN_ARCH >= 10 && PAN_ARCH < 14
-         if (inputs.view_mask) {
+         if (variant_inputs.view_mask) {
             nir_lower_multiview_options options = {
-               .view_mask = inputs.view_mask,
+               .view_mask = variant_inputs.view_mask,
                .allowed_per_view_outputs = ~0
             };
 
@@ -1482,16 +1484,17 @@ panvk_compile_shader(struct panvk_device *dev,
 
          struct pan_varying_layout varying_layout;
          if (v == PANVK_VS_VARIANT_HW) {
-            pan_varying_collect_formats(&varying_layout, nir, inputs.gpu_id);
+            pan_varying_collect_formats(&varying_layout, nir,
+                                        variant_inputs.gpu_id);
             pan_build_varying_layout_compact(&varying_layout, nir,
-                                             inputs.gpu_id);
-            inputs.varying_layout = &varying_layout;
+                                             variant_inputs.gpu_id);
+            variant_inputs.varying_layout = &varying_layout;
          }
 
          variant->own_bin = true;
 
-         result = panvk_compile_nir(dev, nir, info->flags, &inputs, state,
-                                    noperspective_varyings,
+         result = panvk_compile_nir(dev, nir, info->flags, &variant_inputs,
+                                    state, noperspective_varyings,
                                     &shader->desc_info, variant);
 
          /* If we cloned, it's our job to clean up */
